e7da2fe76e8af7e60857fedefd491077f609483a
floxxui <60339661+floxxui@users.noreply.github.com>
2020-12-10 16:24:54 +0100

    Snabb sak bara oops

--- Snek/Program.cs
+++ Snek/Program.cs
@@ -14,22 +14,24 @@ namespace Snek
         static void Main(string[] args)
         {
             //Saker att fixa:
-                //En snake
-                //En spelplan där ex antal pixlar motsvarar en ruta
                 //En random som genererar en matbit till en random ruta
-                //Se till att matbiten hamnar i en ruta, (och inte på flertal rutor)
                 //ormen blir längre ifall man äter en matbit
                 //genererar en ny matbit ifall förra är uppäten
-                //dessa följer efter ormen
+                //lägger till ett block efter ormen om man äter en matbit
                 //Game over om man krockar i väggen
                 //Game over ifall man krockar med sig själv
                 //Poängsystem
 
-
+            //Fixat:
+                //En spelplan där ex antal pixlar motsvarar en ruta
+                //En snake
+                //start, game och game over screen som kan byta mellan varandra
 
 
             Raylib.InitWindow(500, 500, "Snake Window");
             Raylib.SetTargetFPS(60);
+
+            float timer = 2;
 
             //Window w = new Window();
             Snake s = new Snake();
